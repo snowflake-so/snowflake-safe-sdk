f4f3d474a7fcb64614be3f8687ae31d3753bd0e0
Chung Quan Tin <tin@snowflake.so>
2022-07-12 22:23:38 +0700

    Remove redundant code in InstructionBuilder

--- src/utils/ids.ts
+++ src/utils/ids.ts
@@ -3,7 +3,3 @@ import { PublicKey } from "@solana/web3.js";
 export const ASSOCIATED_TOKEN_PROGRAM_ID = new PublicKey(
   "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL"
 );
-
-export const FRIKTION_APP_PROGRAM_ID = new PublicKey(
-  "VoLT1mJz1sbnxwq5Fv2SXjdVDgPXrb9tJyC8WpMDkSp"
-);
